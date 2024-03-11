9388fb4da616746568ee6ebe98e154c7ea436d12
Leandro Santiago <leandro.s4nt1@gmail.com>
2024-03-11 10:22:57 -0400

    Added initial project

---
+++ weeabu/src/main/java/com/solitudeworks/weeabu/WeeabuApplication.java
@@ -0,0 +1,15 @@
+package com.solitudeworks.weeabu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeabuApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WeeabuApplication.class, args);
+
+    }
+
+}
---
+++ weeabu/src/test/java/com/solitudeworks/weeabu/WeeabuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solitudeworks.weeabu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeabuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
